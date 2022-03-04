05c3accf3b7de9c9ed03fe42f4c085d0a674877d
kebabzter <gabzterthesecond@gmail.com>
2022-03-04 18:08:25 +0200

    Cleaned the project!

---
+++ BlueSun/Data/BlueSunDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace BlueSun.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    public class BlueSunDbContext : IdentityDbContext
+    {
+        public BlueSunDbContext(DbContextOptions<BlueSunDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ BlueSun/Program.cs
@@ -0,0 +1,49 @@
+using BlueSun.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BlueSunDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options =>
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+    })
+    .AddEntityFrameworkStores<BlueSunDbContext>();
+
+builder.Services
+    .AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection()
+   .UseStaticFiles()
+   .UseRouting()
+   .UseAuthentication()
+   .UseAuthorization();
+
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+
+app.Run();
