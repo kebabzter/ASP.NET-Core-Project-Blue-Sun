18992f222e2d9b6ba3c16c4a38bc531418da3377
kebabzter <gabzterthesecond@gmail.com>
2022-04-04 14:13:38 +0300

    Fixing last commit(Added missing Model and Details View)

---
+++ BlueSun/Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿namespace BlueSun.Controllers
+{
+    using BlueSun.Data;
+    using BlueSun.Infrastructure.Extensions;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class UsersController : Controller
+    {
+        private readonly BlueSunDbContext data;
+
+        public UsersController(BlueSunDbContext data)
+        {
+            this.data = data;
+        }
+
+        public IActionResult Purchase(int id)
+        {
+            var user = this.data.Users.First(u => u.Id == this.User.Id());
+            var nft = this.data.NFTs.First(n => n.Id == id);
+
+            if (nft.Price > user.Balance)
+            {
+                return RedirectToAction("Details", "NFTs", id);
+            }
+
+            nft.OwnerId = user.Id;
+            user.Balance -= nft.Price;
+
+            this.data.SaveChanges();
+
+            return RedirectToAction("Details", "NFTs", id);
+        }
+
+        //TODO: Finish wallet system if possible!
+        //[HttpGet]
+        //public IActionResult ConnectWallet()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult ConnectWallet()
+        //{
+        //    var user = this.data.Users.First(u => u.Id == this.User.Id());
+
+        //    if (user.HasWallet)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    user.HasWallet = true;
+
+        //    return RedirectToAction("/");
+        //}
+    }
+}
---
+++ BlueSun/Models/NFTs/NFTDetailsViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace BlueSun.Models.NFTs
+{
+    using BlueSun.Data.Models;
+
+    public class NFTDetailsViewModel
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; }
+
+        public decimal Price { get; init; }
+
+        public string OwnerId { get; set; }
+
+        public string OwnerName { get; set; }
+
+        public string ArtistName { get; init; }
+
+        public int ArtistId { get; init; }
+
+        public string Description { get; init; }
+
+        public string ImageUrl { get; init; }
+
+        public int NFTCollectionId { get; init; }
+
+        public string NFTCollectionName { get; init; }
+
+    }
+}
